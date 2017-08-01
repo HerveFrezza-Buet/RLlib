496d69396516cda1bb78521a728352ac1a929ef3
Jeremy Fix <jeremy.fix@gmail.com>
2017-08-01 16:59:56 +0200

    the pendulum with actor-critic TD-Q or LSTD-Q do not work very well .....

--- examples/example-004-004-pendulum-TDQ-softmax-onestep.cc
+++ examples/example-004-004-pendulum-TDQ-softmax-onestep.cc
@@ -26,7 +26,7 @@
 
 // Experiment : pendulum
 // Architecture : RBF features phi(s,a) with continuity in s.
-// Critic : recLSTD-Q 
+// Critic : TD-Q with a linear Q functon over phi(s,a)
 // Actor : Softmax with scores computes as a linear combinations of phi(s,a)
 // Learner : One-step Actor Critic
 
@@ -48,6 +48,7 @@
 using namespace std::placeholders;
 
 // This is our simulator.
+//typedef rl::problem::inverted_pendulum::Simulator<rl::problem::inverted_pendulum::DefaultParam> Simulator;
 
 class DefaultParam {
       public:
@@ -71,31 +72,32 @@ typedef rl::problem::inverted_pendulum::Simulator<DefaultParam> Simulator;
 
 // Let us define the parameters.
 #define paramGAMMA                           0.9
-#define paramREG                            10.0
+#define paramALPHA_V                         0.1
 #define paramALPHA_P                         0.01
+#define paramTEMP                            1.0
 
-// For LSTD-Q, we need to collect some statistics (from transitions)
-// before beginning to update the parameter vector of the greedy policy
-// Here, we collect 5000 transitions which correspond approximately to
-// 300 episodes because initially, the episode length of a random policy
-// is around 15-20 balancing steps
-#define NB_OF_TRANSITIONS_WARMUP  5000
 #define NB_OF_EPISODES            5000
 #define NB_OF_TESTING_EPISODES      50
 #define MAX_EPISODE_LENGTH        3000
 
 
-double fct_p(gsl_vector* tmp, const gsl_vector* theta, const S& s, const A& a) {
+double fct_q(gsl_vector* tmp, const gsl_vector* theta, const S& s, const A& a) {
   double res;
-  phi_rbf(tmp,s,a);              // phi_sa = phi(s,a)
-  gsl_blas_ddot(theta,tmp,&res); // res    = th^T  . phi_sa
+  phi_rbf(tmp, s, a);              // phi_sa = phi(s,a)
+  gsl_blas_ddot(theta, tmp, &res); // res    = th^T  . phi_sa
   return res;
 }
+
+void fct_grad_q(const gsl_vector* theta, gsl_vector* grad, const S& s, const A& a) {
+  phi_rbf(grad, s, a);
+}
+
 
 template<typename AITER>
 std::vector<double> get_action_probabilities(AITER action_begin,
 					     AITER action_end,
 					     unsigned int nb_actions,
+					     double temperature,
 					     gsl_vector* tmp,
 					     const gsl_vector* theta_p,
 					     const S& s) {
@@ -104,7 +106,7 @@ std::vector<double> get_action_probabilities(AITER action_begin,
     double psum = 0.0;
     auto piter = probaActions.begin();
     while(aiter != action_end) {
-      *piter = exp(fct_p(tmp, theta_p, s, *aiter));
+      *piter = exp(fct_q(tmp, theta_p, s, *aiter)/temperature);
       psum += *piter;
       ++aiter;
       ++piter;
@@ -117,6 +119,7 @@ std::vector<double> get_action_probabilities(AITER action_begin,
 template<typename AITER>
 void fct_grad_log_p(AITER action_begin, AITER action_end,
 		    unsigned int nb_actions,
+		    double temperature, 
 		    gsl_vector* tmp,
 		    const gsl_vector* theta_p,
 		    gsl_vector* grad_log_p,
@@ -124,7 +127,7 @@ void fct_grad_log_p(AITER action_begin, AITER action_end,
   gsl_vector_set_zero(grad_log_p);
     
   // We need to get the probabilities of the actions
-  auto probaActions = get_action_probabilities(action_begin, action_end, nb_actions, tmp, theta_p, s);
+  auto probaActions = get_action_probabilities(action_begin, action_end, nb_actions, temperature, tmp, theta_p, s);
   
   // And we can then compute the gradient of ln(Pi)
   // which is : dlog(phi(a/s))/dtheta = phi(s, a) - sum_b pi(b/s) phi(s, b)
@@ -138,6 +141,7 @@ void fct_grad_log_p(AITER action_begin, AITER action_end,
     ++aiter;
     ++piter;
   }
+  gsl_vector_scale(grad_log_p, 1. /temperature);
 }
 
 void save_qfunction(std::string filename,
@@ -147,9 +151,9 @@ void save_qfunction(std::string filename,
   std::ofstream outfile(filename);
   for(unsigned int i = 0 ; i < Ns; ++i){
     auto s = S(-M_PI_2 + M_PI * i / (Ns-1.), 0.);
-    outfile << s.angle << " " << s.speed << " -1 " << fct_p(tmp, theta, s, rl::problem::inverted_pendulum::actionLeft) << std::endl;
-    outfile << s.angle << " " << s.speed << " 0 " << fct_p(tmp, theta, s, rl::problem::inverted_pendulum::actionNone) << std::endl;
-    outfile << s.angle << " " << s.speed <<  " 1 " << fct_p(tmp, theta, s, rl::problem::inverted_pendulum::actionRight) << std::endl;
+    outfile << s.angle << " " << s.speed << " -1 " << fct_q(tmp, theta, s, rl::problem::inverted_pendulum::actionLeft) << std::endl;
+    outfile << s.angle << " " << s.speed << " 0 " << fct_q(tmp, theta, s, rl::problem::inverted_pendulum::actionNone) << std::endl;
+    outfile << s.angle << " " << s.speed <<  " 1 " << fct_q(tmp, theta, s, rl::problem::inverted_pendulum::actionRight) << std::endl;
   }
   outfile.close();
 }
@@ -159,12 +163,13 @@ void save_policy(std::string filename,
 		 int Ns,
 		 AITER action_begin, AITER action_end,
 		 unsigned int nb_actions,
+		 double temperature, 
 		 gsl_vector* tmp,
 		 const gsl_vector* theta_p) {
   std::ofstream outfile(filename);
   for(unsigned int i = 0 ; i < Ns; ++i){
     auto s = S(-M_PI_2 + M_PI * i / (Ns-1.), 0.);
-    auto probaActions = get_action_probabilities(action_begin, action_end, nb_actions, tmp, theta_p, s);
+    auto probaActions = get_action_probabilities(action_begin, action_end, nb_actions, temperature, tmp, theta_p, s);
     
     outfile << s.angle << " " << s.speed << " -1 " << probaActions[1] << std::endl;
     outfile << s.angle << " " << s.speed << " 0 " << probaActions[0] << std::endl;
@@ -172,7 +177,6 @@ void save_policy(std::string filename,
   }
   outfile.close();
 }
-
 
 
 int main(int argc, char* argv[]) {
@@ -188,60 +192,55 @@ int main(int argc, char* argv[]) {
   ////////////// Actor/Critic
   // 2a) Instantiate the Critic, we here use LSTD-Q
   gsl_vector* theta_q = gsl_vector_calloc(PHI_RBF_DIMENSION);
-  auto critic = rl::gsl::LSTDQ<S, A>(theta_q,
-				     paramGAMMA, paramREG,
-				     NB_OF_TRANSITIONS_WARMUP,
-				     phi_rbf); 
+  gsl_vector* tmp = gsl_vector_calloc(PHI_RBF_DIMENSION);
+  
+  auto critic = rl::gsl::td<S, A>(theta_q,
+				  paramGAMMA, paramALPHA_V,
+				  std::bind(fct_q, tmp, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3),
+				  fct_grad_q);
+  
 
   // 2b) Instantiate the Actor
   gsl_vector* theta_p = gsl_vector_calloc(PHI_RBF_DIMENSION);
-  gsl_vector* tmp = gsl_vector_calloc(PHI_RBF_DIMENSION);
-  auto scores = std::bind(fct_p, tmp, theta_p, std::placeholders::_1, std::placeholders::_2);
+  auto scores = std::bind(fct_q, tmp, theta_p, std::placeholders::_1, std::placeholders::_2);
   rl::enumerator<A> action_begin(rl::problem::inverted_pendulum::actionNone);
   rl::enumerator<A> action_end = action_begin+nb_actions;
-  auto policy  = rl::policy::softmax(scores, 1.0,
+  double temperature = paramTEMP;
+  
+  auto policy  = rl::policy::softmax(scores, temperature,
 				     action_begin,action_end);
+  auto greedy_policy  = rl::policy::greedy(scores, action_begin,action_end);
 
   // 2c) And finally the actor/critic
-  auto grad = std::bind(fct_grad_log_p<decltype(action_begin)>, action_begin, action_end, nb_actions, tmp, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4);
+  auto grad = std::bind(fct_grad_log_p<decltype(action_begin)>, action_begin, action_end, nb_actions, temperature, tmp, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4);
   auto actor_critic = rl::gsl::ActorCritic::one_step<S, A>(critic,
-							   theta_p,
-							   paramALPHA_P,
-							   grad);
+  							   theta_p,
+  							   paramALPHA_P,
+  							   grad);
 
   auto save_value_function = [tmp, theta_q](std::string filename) { save_qfunction(filename, 20, tmp, theta_q);
   };
-  auto save_p = [action_begin, action_end, nb_actions, tmp, theta_p](std::string filename) {
-    save_policy(filename, 20, action_begin, action_end, nb_actions, tmp, theta_p);
+  auto save_p = [action_begin, action_end, nb_actions, temperature, tmp, theta_p](std::string filename) {
+    save_policy(filename, 20, action_begin, action_end, nb_actions, temperature, tmp, theta_p);
   };
 
   save_value_function("q0.data");
   save_p("p0.data");
   
   try {
-
-    std::cout << "Warm up " << std::endl;
-    for(episode = 0 ; episode < NB_OF_TRANSITIONS_WARMUP/10; ++episode) {
-      simulator.setPhase(Simulator::phase_type());
-      episode_length = rl::episode::learn(simulator,
-					  policy, critic,
-					  MAX_EPISODE_LENGTH);
-    }
-    std::cout << "Done." << std::endl;
-    std::cout << "Training the actor-critic" << std::endl;
     
     for(episode = 0 ; episode < NB_OF_EPISODES; ++episode) {
       simulator.setPhase(Simulator::phase_type());
-      episode_length = rl::episode::learn(simulator,
-					  policy,actor_critic,
-					  MAX_EPISODE_LENGTH);
+      rl::episode::learn(simulator,
+			 policy,actor_critic,
+			 MAX_EPISODE_LENGTH);
       
       // After each episode, we test our policy for NB_OF_TESTING_EPISODES
       // episodes
       double cumul_episode_length = 0.0;
       for(unsigned int tepi = 0 ; tepi < NB_OF_TESTING_EPISODES; ++tepi) {
 	simulator.setPhase(Simulator::phase_type());
-	cumul_episode_length += rl::episode::run(simulator, policy, MAX_EPISODE_LENGTH);
+	cumul_episode_length += rl::episode::run(simulator, greedy_policy, MAX_EPISODE_LENGTH);
       }
       std::cout << "\r Episode " << episode << " : mean length over " << NB_OF_TESTING_EPISODES << " episodes is " << cumul_episode_length/double(NB_OF_TESTING_EPISODES) << std::string(10, ' ') << std::flush;
       
@@ -251,7 +250,7 @@ int main(int argc, char* argv[]) {
     std::cerr << "Exception caught : " << e.what() << std::endl; 
   }
   std::cout << std::endl;
-
+  
   save_value_function("q1.data");
   save_p("p1.data");
   
